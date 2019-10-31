3672cec94a8d0a35002ac3053f26c51d30e233bb
unknown <jareddrayton@users.noreply.github.com>
2019-10-31 22:11:46 +0000

    Wrote a functioin to convert date format
    The date format provided by the interest rate csv does not play
    well with pandas.

--- main.py
+++ main.py
@@ -1,5 +1,81 @@
 import pandas as pd
+import plotly.express as px
+from datetime import datetime
 
-data = pd.read_csv("Bank Rate  Bank of England Database.csv")
+# Import interest rate data
+interest_rate = pd.read_csv("Bank Rate  Bank of England Database.csv")
 
-print(data.head())
+print(interest_rate.head())
+print(interest_rate.columns)
+
+def date_change(x):
+    
+    months = {
+        "Jan": "January", 
+        "Feb": "February", 
+        "Mar": "March",
+        "Apr": "April",
+        "May": "May",
+        "Jun": "June",
+        "Jul": "July",
+        "Aug": "August",
+        "Sep": "September",
+        "Oct": "October",
+        "Nov": "November",
+        "Dec": "December"}
+    
+    day, month, year = x.split(' ')
+
+    astr = year + months[month] + day
+
+    date_object = datetime.strptime(astr, '%y%B%d')
+
+    return date_object
+
+interest_rate['NewDate'] = interest_rate['Date Changed'].apply(date_change)
+
+
+fig = px.line(interest_rate, x='NewDate', y='Rate', line_shape='vh')
+fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hpi = pd.read_csv("UK-HPI-full-file-2019-06.csv")
+
+print(hpi.columns)
+print(hpi.head())
+
+avg_prices = pd.read_csv("Average-prices-2019-06.csv")
+
+print(avg_prices.columns)
+print(avg_prices.head())
+
+criteria = avg_prices['Region_Name'] == "England"
+
+new = avg_prices[criteria]
+
+new = new[["Date", "Region_Name", "Average_Price"]]
+
+print(new)
+
+
+
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = px.line(new, x='Date', y='Average_Price')
+fig.show()
